5de7fa77a93c648122630e211c3ecee0c94775d4
BelhalK <belhal.karimi@gmail.com>
2020-11-21 18:57:58 +0100

    maj

---
+++ src/components/sections/Upcoming.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import { List } from 'semantic-ui-react'
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+
+const Listexample = () => (
+  <List bulleted>
+    <List.Item>Gaining Access</List.Item>
+    <List.Item>Inviting Friends</List.Item>
+    <List.Item>Benefits</List.Item>
+    <List.Item>Warranty</List.Item>
+  </List>
+)
+
+const Testimonial = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'testimonial section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'testimonial-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Upcoming Events',
+    paragraph: ''
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+    Our umpcoming events are available to our community and include:
+        
+        
+          </div>
+          <div className={tilesClasses}>
+          <Listexample data={sectionHeader} className="center-content" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Testimonial.propTypes = propTypes;
+Testimonial.defaultProps = defaultProps;
+
+export default Testimonial;
